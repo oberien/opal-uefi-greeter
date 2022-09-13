a498a1b0942bb21cb90dd89ec464ef6fd3c893e8
oberien <jaro.fietz@gmx.de>
2022-09-13 16:59:26 +0200

    Small cleanup of old scripts

--- scripts/unvfio_nvme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. nvme_device.sh
-
-if ! lsblk | grep -q "$BLK_NVM"; then
-  echo 1 > /sys/bus/pci/devices/"$PCI_NVM"/remove
-  echo 1 > /sys/bus/pci/rescan
-  modprobe -rv vfio-pci
-fi
